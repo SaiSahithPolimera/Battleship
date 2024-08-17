c108e90d260064996a716a14f46c3a55fd1326b5
Sai Sahith <131525241+SaiSahithPolimera@users.noreply.github.com>
2024-08-17 05:42:51 +0530

    feat: add controller

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import "./styles.css";
 import { header } from "./components/header";
 import { board } from "./components/board";
 import { shipContainer } from "./components/shipContainer";
+import { controller } from "./components/controller";
 
 document.body.appendChild(header());
-const mainContainer = document.createElement("div");
-mainContainer.id = 'gameBoardContainer';
-mainContainer.appendChild(board())
-mainContainer.appendChild(shipContainer())
-document.body.appendChild(mainContainer);
+// const mainContainer = document.createElement("div");
+// mainContainer.id = 'gameBoardContainer';
+// mainContainer.appendChild(board())
+// mainContainer.appendChild(shipContainer())
+document.body.appendChild(controller());
